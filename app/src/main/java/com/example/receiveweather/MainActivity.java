81fcbe14337c41d49ea0ce7d75c38c044b810900
silver5302 <30650439+silver5302@users.noreply.github.com>
2017-08-03 17:05:55 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/receiveweather/MainActivity.java
+++ app/src/main/java/com/example/receiveweather/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
                 //dfkdjflsdf
                 //dfkjskldfjlsdjf
+                //sdfjklsdjflkds
             }
         });
 
