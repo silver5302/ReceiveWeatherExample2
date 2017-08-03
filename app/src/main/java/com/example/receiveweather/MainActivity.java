1e9da545522a89951a4ff49a69b4a4a6bf94541f
silver5302 <gold5302@gmail.com>
2017-08-03 17:00:59 +0900

    작업추가

--- app/src/main/java/com/example/receiveweather/MainActivity.java
+++ app/src/main/java/com/example/receiveweather/MainActivity.java
@@ -32,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), LongWeatherActivity.class);
                 startActivity(intent);
+
+                //dfkdjflsdf
             }
         });
 
